2fd84b9cf340b9c334730f06a6241c6191b758e0
0sumrich <lynchardrich@gmail.com>
2020-07-02 12:42:15 +0100

    fixed db

--- server/run.js
+++ server/run.js
@@ -1,13 +1,15 @@
-const { json } = require('express');
-
 (async () => {
 	const idFromUrl = (str) => str.slice("https://www.youtube.com/watch?v=".length);
 	const { currentIds, addNewVids } = require('./db')
 	const { getRssVideos } = require('./ytApiCalls')
 	const fs = require('fs')
 	const path = require('path')
-	const fullRss = await getRssVideos();
-	const currIds = await currentIds();
-	const vidsWeNeed = fullRss.items.filter(o => !currIds.includes(idFromUrl(o.link))).map(o => idFromUrl(o.link))
-	const newVids = await addNewVids(vidsWeNeed)
+	const writeCsv = require('./writeCsv')
+	const getCsv = require('./getCsv')
+	// const fullRss = await getRssVideos();
+	// const currIds = await currentIds();
+	// const vidsWeNeed = fullRss.items.filter(o => !currIds.includes(idFromUrl(o.link)))
+	// await writeCsv(vidsWeNeed, path.join(__dirname, 'toWrite.csv'))
+	const ids = getCsv(path.join(__dirname, 'toWrite.csv')).map(o => idFromUrl(o.link))
+	const newVids = await addNewVids(ids)
 })();
