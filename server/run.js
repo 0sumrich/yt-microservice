1be4a169fc1aeb7c58e4f268673b6e82b6befd28
0sumrich <lynchardrich@gmail.com>
2020-07-10 09:34:55 +0100

    user deleted video fix

--- server/run.js
+++ server/run.js
@@ -1,15 +1,17 @@
 (async () => {
 	const idFromUrl = (str) => str.slice("https://www.youtube.com/watch?v=".length);
 	const { currentIds, addNewVids } = require('./db')
-	const { getRssVideos } = require('./ytApiCalls')
+	const { getRssVideos, getStats } = require('./ytApiCalls')
 	const fs = require('fs')
 	const path = require('path')
 	const writeCsv = require('./writeCsv')
 	const getCsv = require('./getCsv')
 	// const fullRss = await getRssVideos();
-	// const currIds = await currentIds();
+	const currIds = await currentIds();
+	const stats = await getStats(currIds);
+	debugger
 	// const vidsWeNeed = fullRss.items.filter(o => !currIds.includes(idFromUrl(o.link)))
 	// await writeCsv(vidsWeNeed, path.join(__dirname, 'toWrite.csv'))
-	const ids = getCsv(path.join(__dirname, 'toWrite.csv')).map(o => idFromUrl(o.link))
-	const newVids = await addNewVids(ids)
+	// const ids = getCsv(path.join(__dirname, 'toWrite.csv')).map(o => idFromUrl(o.link))
+	// const newVids = await addNewVids(ids)
 })();
